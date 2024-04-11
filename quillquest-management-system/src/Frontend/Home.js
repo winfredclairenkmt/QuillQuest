9b062a5018fa5c91410f4941e0b901ad6c9f9507
Claire <kizzaeverdeen@gmail>
2024-04-11 11:52:05 +0300

    changed navbar to side bar

--- quillquest-management-system/src/Frontend/Home.js
+++ quillquest-management-system/src/Frontend/Home.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import "./Home.css";
 
 const Home = () => {
-  console.log('home');
+  console.log("home");
   return (
-    <div className='logo' >
-      <h1> QuillQuest Library Management System</h1>
-      {/* <img src={qqlogo} alt="qqlogo" className="qqLogo" /> */}
+    <div className="logo">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <h1 style={{ color: "white" }}>QuillQuest Library Management System</h1>
+      </div>
+      <img
+        src="../Assets/"
+        alt="Logo"
+        style={{
+          maxWidth: "200px",
+          marginTop: "20px",
+          filter: "blur(2px)",
+        }}
+      />
+
     </div>
   );
 };
--- quillquest-management-system/src/Frontend/Navbar.js
+++ quillquest-management-system/src/Frontend/Navbar.js
@@ -10,31 +10,29 @@ function Layout() {
   return (   
     <div>
     <header className="App-header">
-        <div className="main-page">
-          <img src="../Assets" className="bookshelf-image" alt="Bbookshelf"/>
-          <div className="nav">
+    <div className="App">
+      <div className="sidebar">
+        <img src="../Assets" className="bookshelf-image" alt="Bbookshelf"/>
             <ul>
+            <li>
+            <Link to="/Checkout">Check Out</Link>
+          </li>
+          <li>
+            <Link to="/AddUser">Add User</Link>
+          </li>
+          <li>
+            <Link to="/addbook">Add Book</Link>
+          </li>
+          <li>
+            <Link to="/getbook">Get Book</Link>
+          </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+
               {/* <li>
                 <Link to="/all-transactions">All Transactions</Link>
               </li> */}
-              <li>
-                <Link to="/Checkout">Check Out</Link>
-              </li>
-              <li>
-                <Link to="/AddUser">Add User</Link>
-              </li>
-              <li>
-                <Link to="/GetUser">Get User</Link>
-              </li>
-              <li>
-                <Link to="/addbook">Add Book</Link>
-              </li>
-              <li>
-                <Link to="/GetBook">Get Book</Link>
-              </li>
-              <li style={leftStyle }>
-                <Link to="/">Home</Link>
-              </li>
             </ul>
             <Outlet />
           </div>
@@ -44,5 +42,7 @@ function Layout() {
   
   );
 }
-
+ // <div className="content">
+      // </div>
+     
 export default Layout;
